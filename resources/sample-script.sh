3efa7c84b1afa10c8a4bf020fc92b93ffc9170df
Sam Woodman <sam.woodman@noaa.gov>
2022-11-16 18:00:25 -0800

    update sample script

--- resources/sample-script.sh
+++ resources/sample-script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Sample script for scraping and processing real-time data from deployment amlr07-20221025
-
+# Commands to run when you first ssh into the VM to mount the buckets, activate the amlr-gliders conda environment, and set general variables.
 /opt/startup_glider_proc.sh
 conda activate amlr-gliders
 
@@ -9,12 +8,12 @@ BUCKET_PATH=$HOME/amlr-gliders-deployments-dev
 BUCKET_PATH_IMAGERY=$HOME/amlr-imagery-raw-dev
 AMLR_GLIDERS=/opt/amlr-gliders
 
+# Set deployemnt-specific variables
 PROJECT=SANDIEGO
 DEPLOYMENT=amlr07-20221025
-MODE=delayed
+MODE=rt
 
+# Scrape data from the SFMC, convert the binary files to DBA files, and create the trajectory NetCDF files
 $AMLR_GLIDERS/scripts/amlr_scrape_sfmc.py $DEPLOYMENT $PROJECT
 $AMLR_GLIDERS/scripts/amlr_binary_to_dba.py $DEPLOYMENT $PROJECT $MODE $BUCKET_PATH
-$AMLR_GLIDERS/scripts/amlr_process_dba_to_nc.py $DEPLOYMENT $PROJECT $MODE $BUCKET_PATH --write_trajectory --write_acoustics
-
-echo "All done"
+$AMLR_GLIDERS/scripts/amlr_process_dba_to_nc.py $DEPLOYMENT $PROJECT $MODE $BUCKET_PATH --write_trajectory
